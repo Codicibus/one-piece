e621df0604815a69ee71999b07b585e4b01fd44c
123木头人 <1206993746@qq.com>
2021-05-24 00:50:05 +0800

    重新组件库以及样式文件

--- web/src/main.js
+++ web/src/main.js
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
 import App from './App.vue'
-import 'ant-design-vue/dist/antd.css'
+
 import router from './router'
 import store from './store'
 
+// 引入ant-design-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+// css全局样式
+import './styles/index.less'
+import './styles/antd.less'
+import 'nprogress/nprogress.css'
+
 const app = createApp(App)
 app.config.productionTip = false
+
 app.use(router)
 app.use(store)
 app.use(Antd)
