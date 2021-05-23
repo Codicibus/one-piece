a08fd951a7017bc4dd5d40ef2dee957f3761c598
123木头人 <1206993746@qq.com>
2021-05-24 00:51:58 +0800

    整改路由

--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -1,5 +1,48 @@
-// 统一导出路由
+import Layout from '@/layouts/index.vue'
 
-import * as route from '@/pages/login/route'
+const routes = [
+	{ path: '/' },
+	{
+		path: '/login',
+		component: () => import('@/pages/login/index.vue')
+	},
+	{
+		path: '/admin',
+		component: Layout,
+		children: [
+			{
+				name: 'Dashboard',
+				path: 'dashboard',
+				// hidden: true, // 是否展示该路由
+				component: () => import('@/pages/admin/dashboard/index.vue'),
+				meta: {
+					title: '仪表盘',
+					icon: 'el-icon-s-home'
+					// activeMenu: '/admin/dashboard' // 控制默认激活展示的路由
+				}
+			},
+			{
+				name: 'Setting',
+				path: 'setting',
+				// hidden: true, // 是否展示该路由
+				component: () => import('@/pages/admin/setting/index.vue'),
+				meta: {
+					title: '基础设置',
+					icon: 'el-icon-s-home'
+				}
+			},
+			{
+				name: 'Article',
+				path: 'article',
+				// hidden: true, // 是否展示该路由
+				component: () => import('@/pages/admin/article/index.vue'),
+				meta: {
+					title: '文章管理',
+					icon: 'el-icon-s-home'
+				}
+			}
+		]
+	}
+]
 
-export default [route]
+export default routes
