52c187af005b03ba5e2e7c4a9f8b59189ee461b8
123木头人 <1206993746@qq.com>
2021-05-25 20:41:41 +0800

    添加文章管理路由

--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -1,12 +1,16 @@
 import Layout from '@/layouts/index.vue'
+import home from '@/pages/home/index.vue'
+import dashboard from '@/pages/admin/dashboard/index.vue'
 
 const routes = [
-	{ path: '/' },
+	{ path: '/', component: home },
 	{
+		name: 'Login',
 		path: '/login',
 		component: () => import('@/pages/login/index.vue')
 	},
 	{
+		name: 'Admin',
 		path: '/admin',
 		component: Layout,
 		children: [
@@ -14,7 +18,7 @@ const routes = [
 				name: 'Dashboard',
 				path: 'dashboard',
 				// hidden: true, // 是否展示该路由
-				component: () => import('@/pages/admin/dashboard/index.vue'),
+				component: dashboard,
 				meta: {
 					title: '仪表盘',
 					icon: 'el-icon-s-home'
@@ -32,12 +36,43 @@ const routes = [
 				}
 			},
 			{
-				name: 'Article',
-				path: 'article',
+				name: 'ArticleList',
+				path: 'articleList',
 				// hidden: true, // 是否展示该路由
-				component: () => import('@/pages/admin/article/index.vue'),
+				component: () => import('@article/articleList/index.vue'),
 				meta: {
-					title: '文章管理',
+					title: '文章列表',
+					icon: 'el-icon-s-home'
+					// activeMenu: '/admin/dashboard' // 控制默认激活展示的路由
+				}
+			},
+			{
+				name: 'ArticleClassify',
+				path: 'articleClassify',
+				// hidden: true, // 是否展示该路由
+				component: () => import('@article/articleClassify/index.vue'),
+				meta: {
+					title: '文章分类',
+					icon: 'el-icon-s-home'
+				}
+			},
+			{
+				name: 'PagingSetting',
+				path: 'pagingSetting',
+				// hidden: false, // 是否展示该路由
+				component: () => import('@article/pagingSetting/index.vue'),
+				meta: {
+					title: '分页设置',
+					icon: 'el-icon-s-home'
+				}
+			},
+			{
+				name: 'ImagePool',
+				path: 'imagePool',
+				// hidden: true, // 是否展示该路由
+				component: () => import('@article/imagePool/index.vue'),
+				meta: {
+					title: '图片池',
 					icon: 'el-icon-s-home'
 				}
 			}
