368b769365c563425501c7c85b379089b1c83c64
0x1un <aumujun@gmail.com>
2021-06-07 00:45:25 +0800

    support cors

--- backend/opiece/server/initialize/router.go
+++ backend/opiece/server/initialize/router.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -8,6 +9,7 @@ import (
 	"opiece/server/global"
 	"opiece/server/middleware"
 	router2 "opiece/server/router"
+	"time"
 )
 
 func Routers() *gin.Engine {
@@ -15,6 +17,15 @@ func Routers() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(cors.New(
+		cors.Config{
+			AllowAllOrigins: true,
+			AllowCredentials: true,
+			AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
+			AllowHeaders:     []string{"Origin"},
+			ExposeHeaders:    []string{"Content-Length"},
+			MaxAge: 12 * time.Hour,
+		}))
 	publicRouter := router.Group("v1")
 	{
 		router2.InitUserRouter(publicRouter)
