ceaf4a2e497e6814f9027cc097e116f35af3d80c
123木头人 <1206993746@qq.com>
2021-06-07 21:04:48 +0800

    关闭服务端渲染，开启代理

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import usePluginImport from 'vite-plugin-importer'
 import { resolve } from 'path'
+import proxy from './config/proxy'
 
 export default defineConfig({
+	// 在生产中服务时的基本公共路径
 	base: '/',
+	// 服务端渲染
+	ssr: false,
+	server: { proxy },
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
