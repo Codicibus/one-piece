80931057c741c7737d36138d7b775e8de34cf9a0
123木头人 <1206993746@qq.com>
2021-06-12 20:50:46 +0800

    添加组件

--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -1,6 +1,35 @@
 // 引入andt
-import { Typography, Input, BackTop, Form, Button } from 'ant-design-vue'
+import {
+	Typography,
+	Input,
+	BackTop,
+	Form,
+	Button,
+	Layout,
+	Menu,
+	Card,
+	Empty,
+	Statistic,
+	Row,
+	Col,
+	Progress,
+	Breadcrumb
+} from 'ant-design-vue'
+
 // 导入
 export default app => {
-	app.use(Typography).use(Input).use(BackTop).use(Form).use(Button)
+	app.use(Typography)
+	app.use(Input)
+	app.use(BackTop)
+	app.use(Form)
+	app.use(Button)
+	app.use(Layout)
+	app.use(Menu)
+	app.use(Card)
+	app.use(Empty)
+	app.use(Statistic)
+	app.use(Row)
+	app.use(Col)
+	app.use(Progress)
+	app.use(Breadcrumb)
 }
