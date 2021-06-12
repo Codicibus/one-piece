f4d1d375e0238c37340e0789da2743456842d3dc
123木头人 <1206993746@qq.com>
2021-06-12 20:52:06 +0800

    重构全局数据状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 // import request from '@/utils/request'
 import { defineStore } from 'pinia'
-import loginStore from '@/pages/login/store'
-import dashboardStore from '@/pages/admin/dashboard/store'
 
-export default defineStore(loginStore, dashboardStore)
+export default defineStore({
+	id: 'main',
+	state: () => ({
+		loading: false
+	})
+})
