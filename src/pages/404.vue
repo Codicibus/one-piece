256c4a1b5bd85bfaaf50b7768a64ff8baba94030
123木头人 <1206993746@qq.com>
2021-06-12 16:58:12 +0800

    完善NotFound页

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,25 @@
-<template>NotFound</template>
-<script></script>
-<style lang="less"></style>
+<template>
+	<a-empty :image="simpleImage" description="NotFound" class="notFound" />
+</template>
+<script>
+import { defineComponent } from 'vue'
+import { Empty } from 'ant-design-vue'
+export default defineComponent({
+	name: 'NotFound',
+	setup() {
+		return {
+			simpleImage: Empty.PRESENTED_IMAGE_SIMPLE
+		}
+	}
+})
+</script>
+
+<style lang="less">
+.notFound {
+	margin: 0;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>
