ffc823a33d94616e309719d1ed44c34261977e25
123木头人 <1206993746@qq.com>
2021-06-13 23:02:21 +0800

    添加模拟数据

---
+++ src/pages/admin/article/articleList/mock.js
@@ -0,0 +1,63 @@
+export const columns = [
+	{
+		title: '标题',
+		dataIndex: 'title',
+		slots: {
+			customRender: 'title'
+		},
+		ellipsis: true
+	},
+	{
+		title: '分类',
+		dataIndex: 'classify',
+		width: '10%',
+		// 筛选名称
+		filters: [
+			{
+				text: 'one',
+				value: 'one'
+			},
+			{
+				text: 'two',
+				value: 'two'
+			}
+		],
+		slots: {
+			customRender: 'classify'
+		}
+	},
+	{
+		title: '发布时间',
+		dataIndex: 'time',
+		sorter: true,
+		ellipsis: true,
+		slots: {
+			customRender: 'time'
+		}
+	},
+	{
+		title: '操作',
+		dataIndex: 'action',
+		width: '30%',
+		slots: {
+			customRender: 'action'
+		}
+	}
+]
+export const dataSource = [
+	{
+		title: '1',
+		classify: 'John Brown',
+		time: 18
+	},
+	{
+		title: '2',
+		classify: 'John Brown',
+		time: 32
+	},
+	{
+		title: '3',
+		classify: 'John Brown',
+		time: 32
+	}
+]
