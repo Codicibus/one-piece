1e714f541c75703580a00df03e38aae67004e0b9
123木头人 <1206993746@qq.com>
2021-06-15 01:17:53 +0800

    添加信息下拉框

--- src/layouts/components/layoutHeader/index.vue
+++ src/layouts/components/layoutHeader/index.vue
@@ -1,5 +1,61 @@
 <template>
-	<a-layout-header style="background: #fff; padding: 0"></a-layout-header>
+	<a-layout-header class="header">
+		<div class="personal">
+			<a-avatar class="avatar">
+				<template #icon>
+					<UserOutlined />
+				</template>
+			</a-avatar>
+			<a-dropdown>
+				<a class="ant-dropdown-link" @click.prevent>
+					wood
+					<DownOutlined />
+				</a>
+				<template #overlay>
+					<a-menu>
+						<a-menu-item>
+							<a href="javascript:;">个人设置</a>
+						</a-menu-item>
+						<a-menu-divider />
+						<a-menu-item @click="signOut"> 退出登录 </a-menu-item>
+					</a-menu>
+				</template>
+			</a-dropdown>
+		</div>
+	</a-layout-header>
 </template>
 
-<style lang="less" scoped></style>
+<script>
+import { defineComponent } from 'vue'
+import { DownOutlined, UserOutlined } from '@ant-design/icons-vue'
+import { removeToken } from '@/utils/auth'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+	components: {
+		DownOutlined,
+		UserOutlined
+	},
+	setup() {
+		const router = useRouter()
+		const signOut = () => {
+			removeToken()
+
+			router.push('/login')
+		}
+		return { signOut }
+	}
+})
+</script>
+
+<style lang="less" scoped>
+.header {
+	background: #fff;
+	padding: 0 16px;
+}
+.personal {
+	float: right;
+	.avatar {
+		margin-right: 5px;
+	}
+}
+</style>
