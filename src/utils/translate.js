b07079a92fac7b6dfd510b67a867e7c716446ad9
123木头人 <1206993746@qq.com>
2021-06-15 01:12:14 +0800

    完成翻译接口封装

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,22 +1,25 @@
 import axios from 'axios'
 import qs from 'qs'
 import md5 from 'md5'
-
 // 请求配置
 const http = axios.create({
 	timeout: 1000 * 3, // 请求超时
-	baseURL: import.meta.env.VITE_TRANSLATE_URL, // 基础路径
+	// baseURL: import.meta.env.VITE_TRANSLATE_URL, // 基础路径
 	transformRequest: [data => qs.stringify(data)]
 })
-
 const path = '/api/trans/vip/translate'
 const from = 'en'
 const to = 'zh'
 const appid = '20200731000530374'
-const translate = q => {
+const translate = async q => {
 	const salt = Date.now()
-	const sign = md5(appid + q + salt + 'm0sg8AlMb3JxAEJ5uCZY')
-	http.post(path, { q, from, to, appid, salt, sign })
+	let sign = md5(appid + q + salt + 'm0sg8AlMb3JxAEJ5uCZY')
+	const { data } = await http.post(path, { q, from, to, appid, salt, sign })
+	const error_msg =
+		data.error_msg === 'Invalid Access Limit'
+			? '请降低翻译频率，3s后再试'
+			: false
+	const msg = data.trans_result ? data.trans_result[0].dst : error_msg
+	return msg
 }
-
 export default translate
