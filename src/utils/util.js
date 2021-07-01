519a4576fbbf520ceacb3d6604eda0c5d12d7b81
123木头人 <1206993746@qq.com>
2021-07-01 20:05:31 +0800

    完善单位转换工具函数

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,5 @@
 export const bytesToGB = bytes => {
 	if (bytes === 0) return '0 B'
-	const k = 1024
-	let GB = bytes / k
+	let GB = bytes / 1024 / 1024 / 1024
 	return GB
-	//toPrecision(3) 后面保留一位小数，如1.0GB                                                                                                                  //return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
 }
