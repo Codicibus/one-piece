5bb055dcef7ce216377ff057dd85dbf3e76ebf71
123木头人 <1206993746@qq.com>
2021-07-01 20:05:42 +0800

    完善仪表盘页面

--- src/pages/admin/dashboard/index.vue
+++ src/pages/admin/dashboard/index.vue
@@ -1,81 +1,75 @@
 <template>
-	<a-row :gutter="16">
-		<a-col :span="8" class="articleState">
-			<a-card title="Card title" :bordered="false">
-				<p>card content</p>
-			</a-card>
-		</a-col>
-		<a-col :span="8">
-			<a-card title="Card title" :bordered="false">
-				<p>card content</p>
-			</a-card>
-		</a-col>
-		<a-col :span="8">
-			<a-card title="Card title" :bordered="false">
-				<p>card content</p>
-			</a-card>
-		</a-col>
-	</a-row>
 	<a-row :gutter="16" class="serverState">
 		<a-col :span="8">
-			<a-card title="CPU" :bordered="false">
-				<a-progress type="dashboard" :percent="cpu_percent" />
+			<a-card title="CPU" bodyStyle="text-align: center;">
+				<a-progress
+					type="dashboard"
+					:percent="dashboardStore.cpuPercent"
+				/>
 			</a-card>
 		</a-col>
 		<a-col :span="8">
-			<a-card title="内存" :bordered="false">
-				<a-progress type="dashboard" :percent="75" />
-				<div>
-					<!-- <span>{{ totalMemory }}</span> -->
-					/
-					<span></span>
+			<a-card
+				title="内存"
+				bodyStyle="display: flex; justify-content: space-evenly;"
+			>
+				<a-progress
+					type="dashboard"
+					:percent="dashboardStore.RAMPercent"
+				/>
+				<div class="detail">
+					<div>
+						{{ dashboardStore.RAMStat('used') }}
+						<a-divider type="vertical" /> 已使用内容
+					</div>
+					<div>
+						{{ dashboardStore.RAMStat('free') }}
+						<a-divider type="vertical" /> 剩余内容
+					</div>
+					<div>
+						{{ dashboardStore.RAMStat('available') }}
+						<a-divider type="vertical" /> 可用总量
+					</div>
+					<div>
+						{{ dashboardStore.RAMStat('total') }}
+						<a-divider type="vertical" /> 内存总量
+					</div>
 				</div>
 			</a-card>
 		</a-col>
 		<a-col :span="8">
-			<a-card title="网络" :bordered="false">
+			<a-card title="网络" bodyStyle="text-align: center;">
 				<a-progress type="dashboard" :percent="75" />
 			</a-card>
 		</a-col>
 	</a-row>
-	{{ dashboardStore.serveDate }}
 </template>
 
 <script>
-// import { message } from 'ant-design-vue'
-import { computed, defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import socket from '@/utils/socket'
 import useStore from './store'
 let dashboardStore = useStore()
 const getSocket = () => {
-	socket.onopen = () => console.log('socket:onopen')
 	socket.onmessage = evn => {
-		let data = JSON.parse(evn.data)
-		dashboardStore.$state = { serveDate: data }
+		let serveDate = JSON.parse(evn.data)
+		dashboardStore.$state = { ...serveDate }
 	}
-	socket.onclose = () => console.log('socket.onclose')
 }
 export default defineComponent({
 	name: 'Dashboard',
 	setup() {
-		const cpu_percent = computed(() => {
-			let percent = dashboardStore.serveDate.cpu_percent
-			if (percent) {
-				return percent.toFixed(0)
-			} else {
-				return 0
-			}
-		})
-
 		onMounted(() => getSocket())
-		return { getSocket, dashboardStore, cpu_percent }
+		return { getSocket, dashboardStore }
 	}
 })
 </script>
 
 <style lang="less" scoped>
-.articleState {
+.serverState {
 	margin-bottom: 16px;
+	.detail {
+		line-height: 200%;
+	}
 }
-// .serverState;
 </style>
--- src/pages/admin/dashboard/store.js
+++ src/pages/admin/dashboard/store.js
@@ -5,8 +5,40 @@ export default defineStore({
 	id: 'dashboard',
 	state: () => ({
 		loading: false,
-		serveDate: {}
+		cpu_percent: null,
+		mem_stat: {},
+		net_stat: []
 	}),
-	getters: {},
-	actions: {}
+	getters: {
+		cpuPercent() {
+			let percent = this.cpu_percent
+			if (percent) {
+				return Number(percent.toFixed(0))
+			} else {
+				return 0
+			}
+		},
+		RAMPercent() {
+			let percent = this.mem_stat.usedPercent
+			if (percent) {
+				return Number(percent.toFixed(0))
+			} else {
+				return 0
+			}
+		}
+	},
+	actions: {
+		RAMStat(tpye) {
+			switch (tpye) {
+				case 'total':
+					return bytesToGB(this.mem_stat.total).toFixed(2) + ' GB'
+				case 'available':
+					return bytesToGB(this.mem_stat.available).toFixed(2) + ' GB'
+				case 'used':
+					return bytesToGB(this.mem_stat.used).toFixed(2) + ' GB'
+				case 'free':
+					return bytesToGB(this.mem_stat.free).toFixed(2) + ' GB'
+			}
+		}
+	}
 })
