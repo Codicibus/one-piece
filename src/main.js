73f3c8d171ecd815569c14c872d3c1aea79b9a8c
123木头人 <1206993746@qq.com>
2021-07-06 22:50:47 +0800

    新增Markdown编辑器

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import '@/router/intercept' // 路由拦截
 import pinia from './plugins/pinia'
 import Antd from './plugins/antd'
+import editor from './plugins/editor'
 
 // css全局样式
 import './styles/index.less'
@@ -16,6 +17,7 @@ app.config.productionTip = false
 app.use(router)
 app.use(pinia)
 app.use(Antd)
+app.use(editor)
 app.mount('#app')
 if (import.meta.env.MODE === 'production') {
 	console.log('当前是生产环境')
