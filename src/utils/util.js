954e59419bc0ebbd1498764a425578b80ad003cb
123木头人 <1206993746@qq.com>
2021-07-06 22:53:11 +0800

    优化单位转换工具函数

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,5 +1,13 @@
-export const bytesToGB = bytes => {
+export const unitTransform = (bytes, unit) => {
 	if (bytes === 0) return '0 B'
+	let MB = bytes / 1024 / 1024
 	let GB = bytes / 1024 / 1024 / 1024
-	return GB
+	switch (unit) {
+		case 'MB':
+			return MB
+		case 'GB':
+			return GB
+		default:
+			return MB
+	}
 }
