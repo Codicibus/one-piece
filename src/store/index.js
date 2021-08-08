a8c0b68ec2a2dfc66ff85a3eb43062130b6db630
123木头人 <1206993746@qq.com>
2021-08-08 13:56:20 +0800

    删除多余文件

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-// import request from '@/utils/request'
-import { defineStore } from 'pinia'
-
-export default defineStore({
-	id: 'main',
-	state: () => ({
-		loading: false
-	})
-})
--- src/utils/socket.js
+++
@@ -1,5 +0,0 @@
-// import { getToken } from '@/utils/auth'
-// const wsUrl = 'wss://api.amujun.com/v1/dashboard/ws/systat'
-// const token = getToken()
-// const socket = new WebSocket(wsUrl, [token])
-// export default socket
